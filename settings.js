877a634c48dd6089107019fd52234295f5b257cf
annklin <71550518+annklin@users.noreply.github.com>
2023-03-27 14:41:23 -0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const chalk = require("chalk");
 require("dotenv").config();
 let gg = process.env.MODS;
 if (!gg) {
-  gg = "917003213983, 923087880256";
+  gg = "558592935452";
 }
 
 global.owner = gg.split(",");
@@ -11,7 +11,7 @@ global.mongodb = process.env.MONGODB || "none";
 global.prefa = process.env.PREFIX || ".";
 global.ggle = process.env.GOOGLE_API || "none";
 global.wter = process.env.WEATHER_API || "none";
-global.sessionId = process.env.SESSION_ID || "NEZUKO";
+global.sessionId = process.env.SESSION_ID || "AYANAMI";
 global.malid = process.env.MAL_USERNAME || undefined;
 global.malpass = process.env.MAL_PASSWORD || undefined;
 global.port = process.env.PORT || 8080;
