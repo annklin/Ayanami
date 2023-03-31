5c783b9f6695948123c6541e249dd7d1d3be333d
annklin <71550518+annklin@users.noreply.github.com>
2023-03-31 20:16:04 -0300

    Update mods.js

--- Organs/commands/General/mods.js
+++ Organs/commands/General/mods.js
@@ -10,16 +10,15 @@ module.exports = {
     const mod = global.owner;
     const owner = mod.map((mod) => mod + "@s.whatsapp.net");
     let mo = `
-       *🎉 Nᴇᴢᴜᴋᴏ Mᴏᴅs 🎉*\n\n`;
+       *🎉 EU MESMO 🎉*\n\n`;
     for (let i = 0; i < mod.length; i++) {
       const um = await client.username(mod[i] + "@s.whatsapp.net");
-      mo += `\n*👑 Name:* ${um}\n*🎖️ Tag:* @${mod[i].split("@")[0]}\n`;
+      mo += `\n*👑 Nome:* ${um} e uns indonésios ai\n*🎖️ Tag:* @${mod[i].split("@")[0]}\n`;
     }
     let urlll = [
-      "https://static.moewalls.com/videos/preview/2022/neko-nezuko-kimetsu-no-yaiba-preview.mp4",
-      "https://static.moewalls.com/videos/preview/2022/nezuko-lying-on-the-water-kimetsu-no-yaiba-preview.mp4",
-      "https://static.moewalls.com/videos/preview/2022/nezuko-kamado-demon-slayer-preview.mp4",
-      "https://upcdn.io/FW25b6N/raw/y2mate.com%20-%20%20Kamado%20Nezuko%20%20%20Kimetsu%20no%20Yaiba%20%20%20Wallpaper%20Live%204K%20%20%20Wallpaper%20Engine%20%20%20Demon%20slayer%20_360p%20(1).mp4",
+      "https://static.moewalls.com/videos/preview/2022/rei-ayanami-in-the-rain-neon-genesis-evangelion-preview.mp4",
+      "https://static.moewalls.com/videos/preview/2022/rei-ayanami-crying-neon-genesis-evangelion-preview.mp4",
+      "https://static.moewalls.com/videos/preview/2022/rei-ayanami-neon-genesis-evangelion-preview.mp4",
     ];
     let rae = urlll[Math.floor(Math.random() * urlll.length)];
     await client.sendMessage(
