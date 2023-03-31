fd639e75ee75562f8c155e7fb934de016b79f1c4
annklin <71550518+annklin@users.noreply.github.com>
2023-03-31 19:27:14 -0300

    Update slap

--- Organs/commands/Fun/slap.js
+++ Organs/commands/Fun/slap.js
@@ -1,29 +1,29 @@
 const canvacord = require("canvacord");
 module.exports = {
-    name: "slap",
-    alias: ["slap"],
-    usage: `${prefa}slap <mention>`,
+    name: "tapa",
+    alias: ["slap", "tapa"],
+    usage: `${prefa}tapa <mencionar>`,
     react: "🎉",
     category: "Fun",
     start: async (client, m, { command, pushName, groupName, isAdmin, iscreator, mentionByTag, prefix, text, args, quoted, mime }) => {
-    if (!m.quoted && !mentionByTag[0]) return m.reply("*❄️ Tag Someone*")
+    if (!m.quoted && !mentionByTag[0]) return m.reply("*❄️ Marcar alguém*")
     if (m.quoted) {
             try {
                 img = await client.profilePictureUrl(m.quoted.sender, 'image')
             } catch {
-                img = "https://i.pinimg.com/564x/84/09/12/840912dd744e6662ab211b8070b5d84c.jpg"
+                img = "https://i.ibb.co/hc1qDm4/images.png"
             }
         } else {
             try {
                 img = await client.profilePictureUrl(mentionByTag[0], 'image')
             } catch {
-                img = "https://i.pinimg.com/564x/84/09/12/840912dd744e6662ab211b8070b5d84c.jpg"
+                img = "https://i.ibb.co/hc1qDm4/images.png"
             }
         }
         try {
             ppuser = await client.profilePictureUrl(m.sender, 'image')
         } catch {
-            ppuser = 'https://i.pinimg.com/564x/84/09/12/840912dd744e6662ab211b8070b5d84c.jpg'
+            ppuser = 'https://i.ibb.co/hc1qDm4/images.png'
         }
         result = await canvacord.Canvacord.slap(ppuser, img);
         await client.sendMessage(m.from, { image: result }, { quoted: m });
