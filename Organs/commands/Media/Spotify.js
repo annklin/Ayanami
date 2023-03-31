b2bfa0c802a484f9c43d1a61ad69293ae26790a8
annklin <71550518+annklin@users.noreply.github.com>
2023-03-31 14:56:09 -0300

    Update Spotify.js

--- Organs/commands/Media/Spotify.js
+++ Organs/commands/Media/Spotify.js
@@ -2,21 +2,21 @@ const Spotify = require('../../../lib/Spotify')
 require ('../../../settings')
 module.exports = {
     name: "spotify",
-    alias: ["sp"],
+    alias: ["spotify","sp"],
     usage:`${prefa}sp [spotify_link]`,
-    desc: "download songs from spotify",
+    desc: "baixar músicas do spotify",
     category: "Media",
 
     start: async(client, m, { command, prefix, text, args }) => {
 
-   if (!text) return m.reply("Where is the url of spotify?")
+   if (!text) return m.reply("Onde está a url do spotify?")
         const spotify = new Spotify(text)
         const info = await spotify.getInfo()
-        if ((info).error) return void m.reply('Provide a valid spotify track URL, Baka!')
+        if ((info).error) return void m.reply('Forneça um URL válida de uma música, Baka!')
         const { name, artists, album_name, release_date, cover_url } = info
-        const details = `🎧 *Title:* ${name || ''}\n🎤 *Artists:* ${(artists || []).join(
+        const details = `🎧 *Titulo:* ${name || ''}\n🎤 *Artista:* ${(artists || []).join(
             ','
-        )}\n💽 *Album:* ${album_name}\n📆 *Release Date:* ${release_date || ''}`
+        )}\n💽 *Album:* ${album_name}\n📆 *Data de Lançamento:* ${release_date || ''}`
        const response = await client.sendMessage(m.from, { image: { url: cover_url }, caption: details }, { quoted: m })
         const buffer = await spotify.download()
         await client.sendMessage(m.from, { audio: buffer }, { quoted: response })
