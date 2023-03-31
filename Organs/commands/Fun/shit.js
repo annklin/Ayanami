7bd7131030c74b567437e20150ea4b0fe7e25bc5
annklin <71550518+annklin@users.noreply.github.com>
2023-03-31 15:45:59 -0300

    Update shit.js

--- Organs/commands/Fun/shit.js
+++ Organs/commands/Fun/shit.js
@@ -2,7 +2,7 @@ const canvacord = require("canvacord");
 module.exports = {
     name: "shit",
     alias: ["poop"],
-    usage: `${prefa}shit <mention>`,
+    usage: `${prefa}shit <mencionar>`,
     react: "🎉",
     category: "Fun",
     start: async (client, m, { command, pushName, groupName, isAdmin, iscreator, mentionByTag, prefix, text, args, quoted, mime }) => {
@@ -11,14 +11,14 @@ module.exports = {
             try {
                 img = await client.profilePictureUrl(m.quoted.sender, 'image')
             } catch {
-                img = "https://i.pinimg.com/564x/84/09/12/840912dd744e6662ab211b8070b5d84c.jpg"
+                img = "https://i.ibb.co/hc1qDm4/images.png"
             }
             result = await canvacord.Canvacord.shit(img);
         } else if (mentionByTag) {
             try {
                 ppuser = await client.profilePictureUrl(mentionByTag[0] || m.sender, 'image')
             } catch {
-                ppuser = 'https://i.pinimg.com/564x/84/09/12/840912dd744e6662ab211b8070b5d84c.jpg'
+                ppuser = 'https://i.ibb.co/hc1qDm4/images.png'
             }
             result = await canvacord.Canvacord.shit(ppuser);
         } 
