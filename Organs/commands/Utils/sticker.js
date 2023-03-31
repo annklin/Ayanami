280b32d46de2e20108420aa647cd3b861a5f6997
annklin <71550518+annklin@users.noreply.github.com>
2023-03-31 14:41:15 -0300

    Update sticker.js

--- Organs/commands/Utils/sticker.js
+++ Organs/commands/Utils/sticker.js
@@ -2,12 +2,12 @@ const { Sticker, createSticker, StickerTypes } = require('wa-sticker-formatter')
 module.exports = {
     name: "sticker",
     alias: ["sticker","s"],
-    desc: "makes sticker from images/videos/gifs",
+    desc: "faz adesivo de imagens / vídeos / gifs",
     category: "Utils",
     react:"✅",
 
     start: async(client, m,{pushName,body,quoted,mime,text,args,flags}) => {
-       if(!quoted) return await client.sendMessage(m.from,{text:"Reply/tag a image/video"})
+       if(!quoted) return await client.sendMessage(m.from,{text:"Responder/marcar uma imagem/vídeo"})
        flags.forEach((flag) => (text = text.replace(flag, '')))
  
        pack = 'AYANAMI'
@@ -22,11 +22,11 @@ module.exports = {
         let sticker = new Sticker(media, {
             pack: pack, // The pack name
             author: author, // The author name
-            type: flags.includes('--c') || flags.includes('--crop') || flags.includes('--cropped')
+            type: flags.includes('-c') || flags.includes('-cortar') || flags.includes('-cortada')
             ? 'crop'
-            : flags.includes('--s') || flags.includes('--stretch') || flags.includes('--stretched')
+            : flags.includes('-e') || flags.includes('-esticar') || flags.includes('--esticado')
             ? 'default'
-            : flags.includes('--circle')
+            : flags.includes('-circulo')
             ? 'circle'
             : 'full' ,
             categories: ['🤩', '🎉'], // The sticker category
@@ -39,12 +39,12 @@ module.exports = {
         client.sendMessage(m.from, {sticker: buffer}, {quoted: m})
     
         } else if (/video/.test(mime)) {
-            if ((quoted.msg || quoted).seconds > 20) return client.sendMessage(m.from,{text:'🕐 Cannot fetch videos longer than *11 Seconds*'})
+            if ((quoted.msg || quoted).seconds > 20) return client.sendMessage(m.from,{text:'🕐 Não é possível criar com vídeos com mais de *11 segundos*'})
             let media = await quoted.download()
         let sticker = new Sticker(media, {
             pack: pack, // The pack name
             author: author, // The author name
-            type: body.includes("#c")|| body.includes("#crop")? StickerTypes.CROPPED: StickerTypes.FULL, // The sticker type
+            type: body.includes("-c")|| body.includes("-cortar")? StickerTypes.CROPPED: StickerTypes.FULL, // The sticker type
             categories: ['🤩', '🎉'], // The sticker category
             id: '12345', // The sticker id
             quality: 30, // The quality of the output file
@@ -56,7 +56,7 @@ module.exports = {
     
         client.sendMessage(m.from, {sticker: stikk}, {quoted: m})
         } else {
-            client.sendMessage(m.from,{text:"❌ Could not find any Image/Video in context"},{quoted:m})
+            client.sendMessage(m.from,{text:"❌ Não foi possível encontrar nenhuma imagem/vídeo no contexto"},{quoted:m})
             }
     }
 }
