6ef07d2c5d48463db24c9288faec27e4523de957
annklin <71550518+annklin@users.noreply.github.com>
2023-04-18 00:19:29 -0300

    Update add.js

--- Organs/commands/Group/add.js
+++ Organs/commands/Group/add.js
@@ -1,18 +1,20 @@
 require('../../../settings')
-
 module.exports = {
   name: "add", 
-  cmd: ['add'],
-  category: 'group',
-  example: 'Yang Mau Di Add Siapa?',
-  start: async(mom, m, { text, quoted, args }) => {
-    const { sender } = m
+  alias: ["add", "adicionar"],
+	usage:`${prefa}add @tag/citar`,
+	desc: "Adiciona membros ao grupo",
+	category: "Group",
+	react:"✅",
+  start: async(client, m, { text, prefix, isBotAdmin,isAdmin,mentionByTag}) => {
+		if(!isAdmin) return client.sendMessage(m.from,{text:"Este é o comando somente admin"},{quoted:m})
+		if(!isBotAdmin) return m.reply("Faça-me admin para usar este comando")
+		const mention= await mentionByTag
+		if(!isAdmin) return client.sendMessage(m.from,{text:"Este é o comando somente admin"},{quoted:m})
+		let users = await (mention[0]) || m.msg.contextInfo.participant
+		if (!users) return m.reply("❌ Não foi possível localizar nenhum ID/numero de usuário")
+			await client.groupParticipantsUpdate(m.from, [users], "add")
+			await client.sendMessage(m.from,{text:`User adicionado com sucesso ✅`},{quoted:m})
+	},
     
-    let users = m.mentionedJid ? m.mentionedJid[0] : m.quoted ? [m.quoted.sender] : [text.replace(/[^0-9]/g, '')+'@s.whatsapp.net']
-    await mom.groupParticipantsUpdate(m.from, users, 'add').then((res) => global.mess("done", m)).catch((err) => global.mess("error", m))
-  },
-  isGroup: true,
-  isAdmin: true,
-  isBotAdmin: true,
-  isQuery: true
 }
