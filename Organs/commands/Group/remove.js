eb0d97bd6715bae3e52c30e4984b0ae20c654caf
annklin <71550518+annklin@users.noreply.github.com>
2023-04-18 00:17:38 -0300

    Update remove.js

--- Organs/commands/Group/remove.js
+++ Organs/commands/Group/remove.js
@@ -1,20 +1,20 @@
 require ('../../../settings')
 module.exports = {
 	name: "remove",
-	alias: ["remove","boom"],
-	usage:`${prefa}remove @tag/quote`,
-	desc: "remove Member from group",
+	alias: ["remove", "remover"],
+	usage:`${prefa}remove @tag/citar`,
+	desc: "Remove membros do grupo",
 	category: "Group",
 	react:"✅",
 	start: async(client, m, { text, prefix, isBotAdmin,isAdmin,mentionByTag}) => {
-		if(!isAdmin) return client.sendMessage(m.from,{text:"This is admin only command"},{quoted:m})
-		if(!isBotAdmin) return m.reply("Make me admin to use this command")
+		if(!isAdmin) return client.sendMessage(m.from,{text:"Este é o comando somente para admins"},{quoted:m})
+		if(!isBotAdmin) return m.reply("Faça-me admin para usar este comando")
 		const mention= await mentionByTag
-		if(!isAdmin) return client.sendMessage(m.from,{text:"This is admin only command"},{quoted:m})
+		if(!isAdmin) return client.sendMessage(m.from,{text:"Este é o comando somente para admins"},{quoted:m})
 		let users = await (mention[0]) || m.msg.contextInfo.participant
-		if (!users) return m.reply("❌ Couldn't find any userID in context")
+		if (!users) return m.reply("❌ Não foi possível localizar nenhum ID/numero de usuário")
 			await client.groupParticipantsUpdate(m.from, [users], "remove")
-			await client.sendMessage(m.from,{text:`Successfully removed`},{quoted:m})
+			await client.sendMessage(m.from,{text:`Foi ban te conhecer`},{quoted:m})
 	},
     
 }
